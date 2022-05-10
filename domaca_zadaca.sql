00d1596f329f23f1d78e863f0f0fe0f04c67ab52
Marko Skorup <mskorup@gmail.com>
2022-05-10 13:15:36 +0200

    Update domaca_zadaca.sql

--- domaca_zadaca.sql
+++ domaca_zadaca.sql
@@ -1,31 +1,37 @@
-# mysql -uroot < C:\Users\Marko\Documents\GitHub\zadace\domaca_zadaca.sql
+# mysql -uroot < C:\Users\Marko\Documents\GitHub\zadace\domaca_zadaca\domaca_zadaca.sql
+# null je zadana vrijednost, kada ne piše podrazumijeva se
 drop database if exists postolar;
 create database postolar;
 use postolar;
 
 create table obuca(
-    vrsta_obuce varchar(50),
-    velicina_obuce varchar(50),
-    materijal varchar(50),
-    oznaka_obuce varchar(50)
+    sifra int not null primary key auto_increment,
+    vrsta_obuce varchar(30),
+    velicina_obuce decimal(50),
+    materijal varchar(30),
+    oznaka_obuce varchar(20) not null
 );
 
 create table korisnik(
-    ime_prezime varchar(50),
-    vrsta_obuce varchar(50),
-    komada_obuce varchar(50),
-    oznaka_obuce varchar(50)
+    sifra int not null primary key auto_increment,
+    ime_prezime varchar(35),
+    vrsta_obuce varchar(30),
+    komada_obuce int,
+    oznaka_obuce varchar(50) not null
 );
 
 create table popravak(
-    oznaka_obuce varchar(50),
+    sifra int not null primary key auto_increment,
+    oznaka_obuce varchar(20) not null,
     cijena varchar(50),
     vrijeme varchar(50),
     rad_segrta varchar(50)
 );
 
 create table segrt (
+    sifra int not null primary key auto_increment,
     ime_prezime varchar(50),
     popravak varchar(50),
-    broj_popravaka_dnevno varchar(50)
+    broj_popravaka_dnevno varchar(50),
+    oznaka_obuce varchar(20) not null
 );
