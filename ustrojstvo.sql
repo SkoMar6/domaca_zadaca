205045a2f900d991f9f132844cabeed92c66f061
Marko Skorup <mskorup@gmail.com>
2022-05-16 15:31:20 +0200

    Ustrojstvo unos podataka

--- ustrojstvo.sql
+++ ustrojstvo.sql
@@ -1,4 +1,4 @@
-# mysql -uroot --default_character_set=utf8 <C:\Users\Marko\Documents\GitHub\domaca_zadaca\ustrojstvo.sql
+# mysql -uroot --default_character_set=utf8 <C:\Users\Marko\Documents\GitHub\zadace\domaca_zadaca\ustrojstvo.sql
 # sifra int not null primary key auto_increment,
 
 drop database if exists ustrojstvo;
@@ -7,14 +7,14 @@ use ustrojstvo;
 
 create table zupanija (
     sifra int not null primary key auto_increment,
-    naziv varchar(30),
+    naziv varchar(30) not null,
     zupan int not null
 );
 
 create table zupan (
     sifra int not null primary key auto_increment,
-    ime varchar (30),
-    prezime varchar (30)
+    ime varchar (30) not null,
+    prezime varchar (30) not null
 );
 
 create table opcina (
@@ -29,6 +29,85 @@ create table mjesto (
     naziv varchar(30)
 );
 
+
+
 alter table zupanija add foreign key (zupan) references zupan(sifra);
 alter table opcina add foreign key (zupanija) references zupanija(sifra);
 alter table mjesto add foreign key (opcina) references opcina(sifra);
+
+# unos podataka
+
+insert into zupan (ime, prezime)
+values ('Ante','Antić');
+
+insert into zupan (ime, prezime)
+values ('Pero','Perić');
+
+insert into zupan (ime, prezime)
+values ('Hrvoje','Horvat');
+
+
+insert into zupanija (naziv, zupan)
+values ('Osječko-baranjska', 1);
+ 
+insert into zupanija (naziv, zupan)
+values ('Virovitčko-podravska', 2);
+
+insert into zupanija (naziv,zupan)
+values ('Vukovarsko-srijemska', 3);
+
+
+insert into opcina (zupanija, naziv)
+values (1, 'Antunovac');
+
+insert into opcina (zupanija, naziv)
+values (1, 'Bilje');
+
+insert into opcina (zupanija, naziv)
+values (2, 'Čačinci');
+
+insert into opcina (zupanija, naziv)
+values (2, 'Pitomača');
+
+insert into opcina (zupanija, naziv)
+values (3, 'Andrijaševci');
+
+insert into opcina (zupanija, naziv)
+values (3, 'Ivankovo');
+
+
+insert into mjesto (opcina, naziv)
+values (1,'Antunovac');
+
+insert into mjesto (opcina, naziv)
+values (1,'Ivanovac');
+
+insert into mjesto (opcina, naziv)
+values (1, 'Bilje');
+
+insert into mjesto (opcina, naziv)
+values (1, 'Kopačevo');
+
+insert into mjesto (opcina, naziv)
+values (2,'Čačinci');
+
+insert into mjesto (opcina, naziv)
+values (2, 'Paušinci');
+
+insert into mjesto (opcina, naziv)
+values (2, 'Otrovanec');
+
+insert into mjesto (opcina, naziv)
+values (2, 'Pitomača');
+
+insert into mjesto (opcina, naziv)
+values (3, 'Andrijaševci');
+
+insert into mjesto (opcina, naziv)
+values (3, 'Rokovci');
+
+insert into mjesto (opcina, naziv)
+values (3, 'Ivankovo');
+
+insert into mjesto (opcina, naziv)
+values (3, 'Retkovci');
