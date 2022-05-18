6ccc8b484869422c2ee31611faece1e78bf79e6b
Marko Skorup <mskorup@gmail.com>
2022-05-18 13:44:32 +0200

    insert podataka

--- edunovajp26.sql
+++ edunovajp26.sql
@@ -58,3 +58,86 @@ alter table clan add foreign key (polaznik) references polaznik(sifra);
 alter table polaznik add foreign key (osoba) references osoba(sifra);
 
 alter table predavac add foreign key (osoba) references osoba(sifra);
+
+# unos podataka
+# loš način
+# 1 - vrijednost dobivenog primarnog ključa
+insert into smjer values
+(null,'Java programiranje',130,5999.99,500,true);
+
+# bolji način
+# 2 - vrijednost dobivenog primarnog ključa
+insert into smjer (upisnina,naziv,trajanje,certificiran,cijena)
+values(500,'PHP programiranje',130,false,6000);
+
+# primjer dobre prakse
+# 3 - vrijednost dobivenog primarnog ključa
+insert into smjer 
+(sifra,naziv,trajanje,cijena,upisnina,certificiran)
+values 
+(null,'Računovodstvo',100,3000,500,null);
+
+
+# 1
+insert into grupa 
+(sifra,naziv,smjer,datumpocetka,maksimalnopolaznika,predavac)
+values 
+(null,'JP26',1,'2022-04-27',20,null);
+
+
+# 2
+insert into grupa 
+(sifra,naziv,smjer,datumpocetka,maksimalnopolaznika,predavac)
+values
+(null,'PP25',2,'2022-04-27',20,null);
+
+
+
+# 1
+insert into osoba (sifra,ime,prezime,oib,email)
+values (null,'Tomislav','Jakopec',
+null,'tjakopec@gmail.com');
+
+
+# 2 unijeti osobu Shaquille O'Neal
+insert into osoba (sifra,ime,prezime,oib,email)
+values (null,'Shaquille','O''Neal',
+null,'shaki@gmail.com');
+
+# mogućnosti 'O\'Neal', "O'Neal"
+
+# 3 - 14
+insert into osoba (sifra,prezime,ime,oib,email) values
+(null,'Županić','Marko',null,'marko.zupanic8@gmail.com'),
+(null,'Borić','Alen',null,'boricalen@live.com'),
+(null,'Ramač','Dino',null,'dinoramac@gmail.com'),
+(null,'Fratrić','Ivana',null,'ivana.fratric1@gmail.com'),
+(null,'Prgić','Mihael',null,'mihaelp243@gmail.com'),
+(null,'Skorup','Marko',null,'mskorup@gmail.com'),
+(null,'Nađ','Filip',null,'filip.nad100@gmail.com'),
+(null,'Kasapović','Vedran',null,'vedran.kasapovic@gmail.com'),
+(null,'Stanić','Ivan',null,'stambaivan@yahoo.com'),
+(null,'Ivan','Olujić',null,'ivan.olujic.os@gmail.com'),
+(null,'Vukašin','Vaščanin',null,'vukasinvascanin0@gmail.com'),
+(null,'Zdelar','Mihael',null,'mzdelar741@gmail.com');
+
+
+
+# 1,2
+insert into predavac (sifra,osoba,iban)
+values (null,1,null),(null,2,null);
+
+
+# 1 - 12
+insert into polaznik (sifra,osoba, brojugovora) values
+(null,3,null),(null,4,null),(null,5,null),
+(null,6,null),(null,7,null),(null,8,null),
+(null,9,null),(null,10,null),(null,11,null),
+(null,12,null),(null,13,null),(null,14,null);
+
+
+insert into clan(grupa,polaznik) values
+(1,1),(1,2),(1,3),
+(1,4),(1,5),(1,6),
+(1,7),(1,8),(1,9),
+(1,10),(1,11),(1,12);
